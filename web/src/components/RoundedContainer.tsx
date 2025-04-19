6b89936829baecc8749fec92bdf47b5fde5658df
cyoung-colostate <cyoung83@colostate.edu>
2025-04-19 12:47:58 -0400

    No better

---
+++ web/src/components/RoundedContainer.tsx
@@ -0,0 +1,25 @@
+import { Box } from '@mui/material'
+
+export function RoundedContainer(props: { title: string; children?: JSX.Element | JSX.Element[] }) {
+    const { title, children } = props
+    return (
+        <Box
+            flex={1}
+            border={'1px solid black'}
+            borderRadius={2}
+            overflow={'hidden'}
+            boxShadow={2}
+        >
+            <Box
+                bgcolor={'#1976d2'}
+                color={'white'}
+                paddingLeft={2}
+                paddingTop={0.5}
+                paddingBottom={0.5}
+            >
+                {title}
+            </Box>
+            <Box padding={2}>{children}</Box>
+        </Box>
+    )
+}
