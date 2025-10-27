d70cb963dda4a2e6e3750402b8ee9f830de2849e
cyoung-colostate <cyoung83@colostate.edu>
2025-10-26 21:21:16 -0400

    no message

--- web/src/pages/AddEntry.tsx
+++ web/src/pages/AddEntry.tsx
@@ -1,10 +1,25 @@
-import { Box } from '@mui/material'
+import { Box, Step, StepLabel, Stepper } from '@mui/material'
 import { SelectCase } from '../components/AddEntry/SelectCase'
+import { LoadAllCases } from '../components/LoadAllCases/SelectCase'
 
 export function AddEntry() {
+    const steps = ['Identify Case', 'Visitor Information', 'Save Entry']
+
     return (
         <Box>
-            <SelectCase />
+            {/* <Box sx={{ width: '100%' }}>
+                <Stepper
+                    activeStep={1}
+                    alternativeLabel
+                >
+                    {steps.map((label) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+            </Box> */}
+            <LoadAllCases />
         </Box>
     )
 }
