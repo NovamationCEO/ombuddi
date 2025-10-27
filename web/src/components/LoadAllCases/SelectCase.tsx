121684f4b9ee98068dd5267dc906319d6202f69e
cyoung-colostate <cyoung83@colostate.edu>
2025-10-26 21:20:49 -0400

    no message

---
+++ web/src/components/LoadAllCases/SelectCase.tsx
@@ -0,0 +1,39 @@
+import { Box } from '@mui/material'
+import { useGetter } from '../../tools/db_tools/useGetter'
+import { CaseType } from '../../types/majorTypes'
+import { Add, Commit } from '@mui/icons-material'
+import { CaseCardThin } from './CaseCardThin'
+import { CaseCard } from './CaseCard'
+
+export function SelectCase() {
+    const casesRes = useGetter<CaseType[]>(['get_all_cases'])
+
+    return (
+        <Box
+            display={'grid'}
+            gap={2}
+            gridTemplateColumns={{
+                xs: '1fr',
+                md: 'repeat(2, minmax(0, 1fr))',
+            }}
+        >
+            <CaseCardThin
+                Icon={<Add />}
+                text={'Add Case'}
+                link={'/add_case'}
+            />
+            <CaseCardThin
+                Icon={<Commit />}
+                text={'Log Without Case'}
+                link={'/log_without_case'}
+            />
+
+            {casesRes.data?.map((caseItem) => (
+                <CaseCard
+                    key={caseItem.id}
+                    caseItem={caseItem}
+                />
+            ))}
+        </Box>
+    )
+}
