75671c0e037fe9b45f3f3b0d72f93069ae125375
cyoung-colostate <cyoung83@colostate.edu>
2025-10-26 21:20:23 -0400

    no message

---
+++ web/src/components/LoadAllCases/CaseCardThin.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material'
+import { RoundButton } from '../../trusted-components/RoundButton'
+import { CaseCardWrapper } from './CaseCardWrapper'
+import { useNavigate } from 'react-router-dom'
+import { ReactElement } from 'react'
+
+export function CaseCardThin(props: { Icon: ReactElement; text: string; link: string }) {
+    const { Icon, text, link } = props
+    const navigate = useNavigate()
+    return (
+        <CaseCardWrapper>
+            <Box
+                display={'flex'}
+                alignItems={'center'}
+                flex={1}
+                height={'100%'}
+                onClick={() => navigate(link)}
+            >
+                <Box m={2}>
+                    <RoundButton size={41}>{Icon}</RoundButton>
+                </Box>
+                <Typography variant="h6">{text}</Typography>
+            </Box>
+        </CaseCardWrapper>
+    )
+}
